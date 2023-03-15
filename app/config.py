69e4e0f5378a93e5381ef72e6042477c7417ec5e
shypilovd1986 <shypilovd@gmail.com>
2023-03-15 10:20:03 +0200

    modify config.py

--- app/config.py
+++ app/config.py
@@ -2,8 +2,15 @@
 import os
 
 
-class Config(object):
+class Config:
     """describes main config for app"""
-    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xe06,e\xc0o\xbf\x03\xeftz\x18\xc7\x03\x94\x82\xa4\x82)9'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+        b'\xe06,e\xc0o\xbf\x03\xeftz\x18\xc7\x03\x94\x82\xa4\x82)9'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:19865421@localhost/items_storage'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    def __repr__(self):
+        return "<class Config>"
+
+    def __str__(self):
+        return "<class Config with configuration app>"
